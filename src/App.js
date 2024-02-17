0cf0c0dbedc7f2059d62b182142ad30d2b8a0f76
KoniChivaJS <mredik200@gmail.com>
2024-02-17 15:09:14 +0200

    Add react skeleton for Pizzas

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import Header from "./components/Header";
 import Categories from "./components/Categories";
 import Sort from "./components/Sort";
 import PizzaBlock from "./components/PizzaBlock";
+import Skeleton from "./components/PizzaBlock/Skeleton";
 import { useEffect, useState } from "react";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://65c7cc54e7c384aada6ef7f5.mockapi.io/items")
       .then((res) => res.json())
-      .then((json) => setItems(json));
+      .then((json) => {
+        setItems(json);
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -26,9 +31,9 @@ function App() {
           </div>
           <h2 className="content__title">Всі піци</h2>
           <div className="content__items">
-            {items.map((obj) => (
-              <PizzaBlock {...obj} key={obj.id} />
-            ))}
+            {isLoading
+              ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
+              : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
           </div>
         </div>
       </div>
