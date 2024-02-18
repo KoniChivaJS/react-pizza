e9316f6564e90a22f944c56aa4ab437430d17015
KoniChivaJS <mredik200@gmail.com>
2024-02-18 15:26:37 +0200

    Add cart page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,10 @@ const Home = () => {
         setItems(json);
         setIsLoading(false);
       });
+    window.scrollTo(0, 0);
   }, []);
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -28,7 +29,7 @@ const Home = () => {
           ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
           : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
       </div>
-    </>
+    </div>
   );
 };
 
