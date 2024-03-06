69bfa5547517cfd1d71ae54738fa037cd42ceba4
KoniChivaJS <mredik200@gmail.com>
2024-03-06 17:47:15 +0200

    Add search pizzas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import { useEffect, useState } from "react";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -17,20 +17,29 @@ const Home = () => {
 
   useEffect(() => {
     setIsLoading(true);
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const sort = sortType.sortProperty;
+    const Desc = isDesc ? "desc" : "asc";
+    const search = searchValue ? `&search=${searchValue}` : "";
     fetch(
-      `https://65c7cc54e7c384aada6ef7f5.mockapi.io/items?${
-        categoryId > 0 ? `category=${categoryId}` : ""
-      }&sortBy=${sortType.sortProperty}&order=${isDesc ? "desc" : "asc"}`
+      `https://65c7cc54e7c384aada6ef7f5.mockapi.io/items?${category}&sortBy=${sort}&order=${Desc}${search}`
     )
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return [];
+      })
       .then((json) => {
         setItems(json);
         setIsLoading(false);
       });
 
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, isDesc]);
-  console.log(sortType);
+  }, [categoryId, sortType, isDesc, searchValue]);
+
+  const pizzas = items.map((obj) => <PizzaBlock {...obj} key={obj.id} />);
+  const skeletons = [...new Array(6)].map((_, i) => <Skeleton key={i} />);
   return (
     <div className="container">
       <div className="content__top">
@@ -46,11 +55,7 @@ const Home = () => {
         />
       </div>
       <h2 className="content__title">Всі піци</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, i) => <Skeleton key={i} />)
-          : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
     </div>
   );
 };
