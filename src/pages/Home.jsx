1feeff50db03e0d0e9c3ffdf3ff1c9ed9e181baf
KoniChivaJS <mredik200@gmail.com>
2024-03-06 18:30:21 +0200

    Add pagination

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,12 @@ import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import { useEffect, useState } from "react";
+import Pagination from "../components/Pagination";
 
 const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-
+  const [currentPage, setCurrentPage] = useState(1);
   const [categoryId, setCategoryId] = useState(0);
   const [sortType, setSortType] = useState({
     name: "популярністю",
@@ -22,7 +23,7 @@ const Home = ({ searchValue }) => {
     const Desc = isDesc ? "desc" : "asc";
     const search = searchValue ? `&search=${searchValue}` : "";
     fetch(
-      `https://65c7cc54e7c384aada6ef7f5.mockapi.io/items?${category}&sortBy=${sort}&order=${Desc}${search}`
+      `https://65c7cc54e7c384aada6ef7f5.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sort}&order=${Desc}${search}`
     )
       .then((res) => {
         if (res.ok) {
@@ -36,7 +37,7 @@ const Home = ({ searchValue }) => {
       });
 
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, isDesc, searchValue]);
+  }, [categoryId, sortType, isDesc, searchValue, currentPage]);
 
   const pizzas = items.map((obj) => <PizzaBlock {...obj} key={obj.id} />);
   const skeletons = [...new Array(6)].map((_, i) => <Skeleton key={i} />);
@@ -56,6 +57,7 @@ const Home = ({ searchValue }) => {
       </div>
       <h2 className="content__title">Всі піци</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 };
